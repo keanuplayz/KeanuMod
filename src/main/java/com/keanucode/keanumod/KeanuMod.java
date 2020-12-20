c4a503314e49cc362030e1c50f6071f4cc38303b
Keanu <keanutimmermans8@gmail.com>
2020-12-20 19:53:20 +0100

    Added a custom item.

---
+++ src/main/java/com/keanucode/keanumod/KeanuMod.java
@@ -0,0 +1,17 @@
+package com.keanucode.keanumod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class KeanuMod implements ModInitializer {
+    public static final Item TRAV = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    @Override
+    public void onInitialize() {
+        System.out.println("heujjjjj!!! goeie zoooi!!!!!");
+        Registry.register(Registry.ITEM, new Identifier("keanumod", "trav"), TRAV);
+    };
+}
